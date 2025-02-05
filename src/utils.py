358f6047a3d031d3b0fd6337912bdf798509dd6e
shenghao <1249646094@qq.com>
2025-02-05 22:05:54 +0000

    Add YOLOv5

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,8 @@ sys.path.append(str(src_path))
 
 from model_hub import LeNet, Net
 from models_cv import *
-
+from YOLOv5.yolo import *
+from YOLOv5.datasets import *
 
 class Logger(object):
     level_relations = {
@@ -157,21 +158,22 @@ def collate_fn(batch):
     return images, targets
 
 # Load COCO dataset
-def load_COCO(batch_size=32, root='/path/to/coco', num_workers=2):
+def load_COCO_old(batch_size=32, root='/home/shenghao/torch-deepimportance/yolov5/data/coco', num_workers=2):
     # Define paths to annotations
     ann_train = os.path.join(root, 'annotations', 'instances_train2017.json')
     ann_val = os.path.join(root, 'annotations', 'instances_val2017.json')
 
     # Define image directories
-    img_dir_train = os.path.join(root, 'train2017')
-    img_dir_val = os.path.join(root, 'val2017')
+    img_dir_train = os.path.join(root, 'images', 'train2017')
+    img_dir_val = os.path.join(root, 'images', 'val2017')
 
     # Define transformations
+    # 1, 3, 640, 640
     transform = transforms.Compose([
-        transforms.Resize((640, 640)),  # Resize images to 640x640
-        transforms.ToTensor(),          # Convert images to PyTorch tensors
+        transforms.Resize((640, 640)),
+        transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             std=[0.229, 0.224, 0.225])  # Normalize with ImageNet means and stds
+                             std=[0.229, 0.224, 0.225])
     ])
 
     # Load datasets
@@ -198,6 +200,78 @@ def load_COCO(batch_size=32, root='/path/to/coco', num_workers=2):
     ]
 
     return train_loader, val_loader, classes
+
+## TODO: cfg and model_stride should be passed as arguments
+def load_COCO(batch_size=32, data_path='../data/elephant.yaml', img_size=[640, 640], 
+              cfg='models/yolov5s.yaml',
+              model_stride=[8, 16, 32],
+              single_cls=True, 
+              cache_images=True, 
+              rect=True, 
+              num_workers=2):
+    
+    hyp = {'lr0': 0.01,  # initial learning rate (SGD=1E-2, Adam=1E-3)
+       'momentum': 0.937,  # SGD momentum
+       'weight_decay': 5e-4,  # optimizer weight decay
+       'giou': 0.05,  # giou loss gain
+       'cls': 0.58,  # cls loss gain
+       'cls_pw': 1.0,  # cls BCELoss positive_weight
+       'obj': 1.0,  # obj loss gain (*=img_size/320 if img_size != 320)
+       'obj_pw': 1.0,  # obj BCELoss positive_weight
+       'iou_t': 0.20,  # iou training threshold
+       'anchor_t': 4.0,  # anchor-multiple threshold
+       'fl_gamma': 0.0,  # focal loss gamma (efficientDet default is gamma=1.5)
+       'hsv_h': 0.014,  # image HSV-Hue augmentation (fraction)
+       'hsv_s': 0.68,  # image HSV-Saturation augmentation (fraction)
+       'hsv_v': 0.36,  # image HSV-Value augmentation (fraction)
+       'degrees': 0.0,  # image rotation (+/- deg)
+       'translate': 0.0,  # image translation (+/- fraction)
+       'scale': 0.5,  # image scale (+/- gain)
+       'shear': 0.0}  # image shear (+/- deg)
+    
+    data_path = glob.glob('./**/' + data_path, recursive=True)[0]
+    with open(data_path) as f:
+        data_dict = yaml.load(f, Loader=yaml.FullLoader)  # model dict
+    train_path = data_dict['train']
+    test_path = data_dict['val']
+    nc = 1 if single_cls else int(data_dict['nc'])
+
+    # Image sizes
+    gs = int(max(model_stride))  # grid size (max stride)
+    if any(x % gs != 0 for x in img_size):
+        print('WARNING: --img-size %g,%g must be multiple of %s max stride %g' % (*img_size, cfg, gs))
+    imgsz, imgsz_test = [make_divisible(x, gs) for x in img_size]  # image sizes (train, test)
+
+    dataset = LoadImagesAndLabels(train_path, imgsz, batch_size,
+                                  augment=True,
+                                  hyp=hyp,  # augmentation hyperparameters
+                                  rect=rect,  # rectangular training
+                                  cache_images=cache_images,
+                                  single_cls=single_cls)
+    
+    trainloader = torch.utils.data.DataLoader(dataset,
+                                             batch_size=batch_size,
+                                             num_workers=num_workers,
+                                             shuffle=not rect,  # Shuffle=True unless rectangular training is used
+                                             pin_memory=True,
+                                             collate_fn=dataset.collate_fn)
+
+    # Testloader
+    testloader = torch.utils.data.DataLoader(LoadImagesAndLabels(test_path, imgsz_test, batch_size,
+                                                                 hyp=hyp,
+                                                                 rect=True,
+                                                                 cache_images=cache_images,
+                                                                 single_cls=single_cls),
+                                             batch_size=batch_size,
+                                             num_workers=num_workers,
+                                             pin_memory=True,
+                                             collate_fn=dataset.collate_fn)
+
+    # class frequency
+    labels = np.concatenate(dataset.labels, 0)
+    c = torch.tensor(labels[:, 0])  # classes
+
+    return trainloader, testloader, c
 
 # Load the ImageNet dataset
 def load_ImageNet(batch_size=32, root='/data/shenghao/dataset/ImageNet', num_workers=2, use_val=False):
@@ -586,4 +660,4 @@ if __name__ == '__main__':
     # test_model()
     # test_cifar_models()
     # test_selector_dataloader(root = args.data_path)
-
+    load_COCO()
